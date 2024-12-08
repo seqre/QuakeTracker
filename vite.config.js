24cef2b742e2fb865e90dfa05f19bbb01314b7a8
Marek 'seqre' Grzelak <git@seqre.dev>
2024-12-08 23:34:47 +0100

    meta: update config according to docs

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,40 @@
-import { defineConfig } from "vite";
-import { sveltekit } from "@sveltejs/kit/vite";
+import {defineConfig} from "vite";
+import {sveltekit} from "@sveltejs/kit/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [sveltekit()],
+    plugins: [sveltekit()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 1421,
+            }
+            : undefined,
+        watch: {
+            // 3. tell vite to ignore watching `src-tauri`
+            ignored: ["**/src-tauri/**"],
+        },
+        // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+        envPrefix: ['VITE_', 'TAURI_ENV_*'],
+        build: {
+            // don't minify for debug builds
+            minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+            // produce sourcemaps for debug builds
+            sourcemap: !!process.env.TAURI_ENV_DEBUG,
+        },
     },
-  },
 }));
