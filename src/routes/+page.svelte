0bd27ade772f26b0f6af8ec706e49e3c46c8868f
Marek 'seqre' Grzelak <git@seqre.dev>
2024-12-08 23:25:14 +0100

    chore: format frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,117 +1,105 @@
 <script lang="ts">
 
-import maplibregl from 'maplibre-gl';
-import { PMTiles, Protocol } from 'pmtiles';
-  import { onMount } from 'svelte';
+    import maplibregl from 'maplibre-gl';
+    import {PMTiles, Protocol} from 'pmtiles';
+    import {onMount} from 'svelte';
 
-  import { defaultTheme } from '../theme'
-  
-let { data }: { data: PageData } = $props();
- 
+    import {defaultTheme} from '../theme'
 
-onMount(async () => {
-  const protocol = new Protocol();
-      maplibregl.addProtocol("pmtiles", (request) => {
-        return new Promise((resolve, reject) => {
-          const callback = (err: any, data: any) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve({ data });
-            }
-          };
+    let {data}: { data: PageData } = $props();
 
-          protocol.tile(request, callback);
+
+    onMount(async () => {
+        const protocol = new Protocol();
+        maplibregl.addProtocol("pmtiles", (request) => {
+            return new Promise((resolve, reject) => {
+                const callback = (err: any, data: any) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({data});
+                    }
+                };
+
+                protocol.tile(request, callback);
+            });
         });
-      });
 
-      // the location of our pmtiles file
-      const PMTILES_URL = "./my_area.pmtiles";
+        // the location of our pmtiles file
+        const PMTILES_URL = "./my_area.pmtiles";
 
-      // create a new PmTiles instance
-      const pmtilesInstance = new PMTiles(PMTILES_URL);
+        // create a new PmTiles instance
+        const pmtilesInstance = new PMTiles(PMTILES_URL);
 
-      // this is so we share one instance across the JS code and the map renderer
-      protocol.add(pmtilesInstance);
+        // this is so we share one instance across the JS code and the map renderer
+        protocol.add(pmtilesInstance);
 
-      // we first fetch the header so we can get the center lon, lat of the map.
-      const mapMetaData = await pmtilesInstance.getHeader();
-
+        // we first fetch the header so we can get the center lon, lat of the map.
+        const mapMetaData = await pmtilesInstance.getHeader();
 
 
-    
-    const all = await fetch('https://www.seismicportal.eu/fdsnws/event/1/query?limit=100&format=json')
-    .then(res => res.json())
-    .then(res => res.features)
+        const all = await fetch('https://www.seismicportal.eu/fdsnws/event/1/query?limit=100&format=json')
+            .then(res => res.json())
+            .then(res => res.features)
 
-    console.log(all)
-
-
-
-
-
-
-    
+        console.log(all)
 
 
         const map = new maplibregl.Map({
-              container: 'map',
-              minZoom: 1.3,
-              maxZoom: 5,
-              center: [0, 0],
-              zoom: 0.2,
-              style: {
-                      version: 8,
-                      sprite: "https://demotiles.maplibre.org/styles/osm-bright-gl-style/sprite",
-                      // ading protomaps as the data source for our map
-                      sources: {
-                          'example-points': {
-                      type: 'geojson',
-                      data: {
-                          type: 'FeatureCollection',
-                          features: data.features,
-                            }
-                        },
-            
-                          'protomaps': {
-                              type: 'vector',
-                              url: `pmtiles://${PMTILES_URL}`,
-                              attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a>'
-                          }
-                      },
-                       glyphs: 'https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf',
-                      // simple map layer definitions
-                      // for more information about structure see
-                      // https://maplibre.org/maplibre-style-spec/layers/
-                      layers: [...defaultTheme, 
-                      {
-                          id: "example-points-layer",
-                          type: "circle",
-                          source: "example-points",
-                          paint: {
+            container: 'map',
+            minZoom: 1.3,
+            maxZoom: 5,
+            center: [0, 0],
+            zoom: 0.2,
+            style: {
+                version: 8,
+                sprite: "https://demotiles.maplibre.org/styles/osm-bright-gl-style/sprite",
+                // ading protomaps as the data source for our map
+                sources: {
+                    'example-points': {
+                        type: 'geojson',
+                        data: {
+                            type: 'FeatureCollection',
+                            features: data.features,
+                        }
+                    },
+
+                    'protomaps': {
+                        type: 'vector',
+                        url: `pmtiles://${PMTILES_URL}`,
+                        attribution: '© <a href="https://openstreetmap.org">OpenStreetMap</a>'
+                    }
+                },
+                glyphs: 'https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf',
+                // simple map layer definitions
+                // for more information about structure see
+                // https://maplibre.org/maplibre-style-spec/layers/
+                layers: [...defaultTheme,
+                    {
+                        id: "example-points-layer",
+                        type: "circle",
+                        source: "example-points",
+                        paint: {
                             "circle-radius": 5,
                             "circle-color": "#ff5722",
-                          },
                         },
-                      ]
-                  
-                  }
-          });
+                    },
+                ]
 
-  
-
+            }
+        });
 
 
-          map.on('load',async () => {
+        map.on('load', async () => {
             const image = await map.loadImage('https://upload.wikimedia.org/wikipedia/commons/7/7c/201408_cat.png');
             map.addImage('marker-15', image.data);
 
 
             console.log(map)
-          })
+        })
 
 
-          map.on('click', 'example-points-layer', (e) => {
+        map.on('click', 'example-points-layer', (e) => {
             const coordinates = e.features[0]!.geometry.coordinates.slice();
             const description = e.features[0]!.properties.description;
 
@@ -138,15 +126,11 @@ onMount(async () => {
             map.getCanvas().style.cursor = '';
         });
 
-      
-
-
 
         // console.debug(map.styles.features)
 
         // map.on('')  
-})
-
+    })
 
 
 </script>
