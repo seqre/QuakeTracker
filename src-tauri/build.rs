6bbf9359f0717c4ee193db2cd7344254ae8817bf
Marek 'seqre' Grzelak <git@seqre.dev>
2024-12-10 23:51:34 +0100

    chore: extract setup to separate func

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,5 @@
 fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
+
     tauri_build::build()
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use tauri::{App, Runtime};
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri_plugin_log::{Target, TargetKind};
 
@@ -7,6 +9,11 @@ pub fn run() {
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         // .invoke_handler(tauri::generate_handler![])
+        .setup(setup)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn setup<R: Runtime>(app: &mut App<R>) -> Result<(), Box<dyn Error>> {
+    Ok(())
 }
