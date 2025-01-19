746db85e6cb5b93c37b958668223b51e99985799
Marek 'seqre' Grzelak <git@seqre.dev>
2025-01-19 17:23:47 +0100

    fix: use bar chart for magnitudes & fix typos

---
+++ src/charts/magnitude-distribution/options.ts
@@ -0,0 +1,33 @@
+export const magDistributionOption = (magnitude: any) => {
+    return {
+        title: {
+            text: "Magnitude distribution",
+        },
+        xAxis: {
+            name: "Mag",
+            minorTick: {
+                show: true,
+                splitNumber: 5,
+            },
+        },
+        yAxis: {
+            name: "Count",
+        },
+        tooltip: {
+            trigger: "axis",
+            axisPointer: {
+                type: "none",
+            },
+        },
+        grid: {
+            right: '11%',
+        },
+        series: [
+            {
+                name: "Count",
+                data: magnitude,
+                type: "bar",
+            },
+        ],
+    };
+};
--- src/components/RightSidebar.svelte
+++ src/components/RightSidebar.svelte
@@ -1,54 +1,54 @@
 <script lang="ts">
-  import { X } from "lucide-svelte";
-  import * as echarts from "echarts";
-  import { pieRegions } from "../charts/pie-regions/options";
-  import { magDistributionOption } from "../charts/magnitute-distribution/options";
-  import { onMount } from "svelte";
-  import { mapToPieRegions } from "../charts/pie-regions/map";
-  import { invoke } from "@tauri-apps/api/core";
+    import {X} from "lucide-svelte";
+    import * as echarts from "echarts";
+    import {pieRegions} from "../charts/pie-regions/options";
+    import {magDistributionOption} from "../charts/magnitude-distribution/options";
+    import {onMount} from "svelte";
+    import {mapToPieRegions} from "../charts/pie-regions/map";
+    import {invoke} from "@tauri-apps/api/core";
 
-  let { rightSidebar, data } = $props()
+    let {rightSidebar, data} = $props()
 
-  let chartDom: HTMLElement;
-  let chartDom2: HTMLElement;
+    let chartDom: HTMLElement;
+    let chartDom2: HTMLElement;
 
-  let magnitude = $state({});
-  let count_by_year = $state("dupa");
-
-
-  const pieRegionsData = mapToPieRegions(data.features);
+    let magnitude = $state({});
+    let count_by_year = $state("dupa");
 
 
-onMount(async () => {
-    let chart = echarts.init(chartDom);
-    let chart2 = echarts.init(chartDom2);
-
-    magnitude = await invoke("get_magnitude_distribution");
-
-    const magDistributionOptionObj = magDistributionOption(magnitude);
-    const pieRegionsObj = pieRegions(pieRegionsData);
+    const pieRegionsData = mapToPieRegions(data.features);
 
 
-    pieRegionsObj && chart.setOption(pieRegionsObj);
-    magDistributionOptionObj && chart2.setOption(magDistributionOptionObj);
+    onMount(async () => {
+        let chart = echarts.init(chartDom);
+        let chart2 = echarts.init(chartDom2);
 
-    count_by_year = JSON.stringify(await invoke("get_count_by_year"));
-})
+        magnitude = await invoke("get_magnitude_distribution");
+
+        const magDistributionOptionObj = magDistributionOption(magnitude);
+        const pieRegionsObj = pieRegions(pieRegionsData);
+
+
+        pieRegionsObj && chart.setOption(pieRegionsObj);
+        magDistributionOptionObj && chart2.setOption(magDistributionOptionObj);
+
+        count_by_year = JSON.stringify(await invoke("get_count_by_year"));
+    })
 
 </script>
 
 
 <aside
-  class:hidden={rightSidebar}
-  class=" pt-5 h-full fixed bg-white right-0 top-0 z-50 shadow-lg overflow-scroll"
+        class:hidden={rightSidebar}
+        class=" pt-5 h-full fixed bg-white right-0 top-0 z-50 shadow-lg overflow-scroll"
 >
-  <button onclick={() => (rightSidebar = !rightSidebar)} class="absolute right-3 top-2">
-    <X />
-  </button>
+    <button onclick={() => (rightSidebar = !rightSidebar)} class="absolute right-3 top-2">
+        <X/>
+    </button>
 
-  <div class=" w-96 overflow-y-scroll">
-    <div class="w-96 pr-2 h-96" bind:this={chartDom2} id="chart2"></div>
+    <div class=" w-96 overflow-y-scroll">
+        <div class="w-96 pr-2 h-96" bind:this={chartDom2} id="chart2"></div>
 
-    <div style:height="80rem" class="chart w-96" bind:this={chartDom} id="chart"></div>
-  </div>
+        <div style:height="80rem" class="chart w-96" bind:this={chartDom} id="chart"></div>
+    </div>
 </aside>
